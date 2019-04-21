370c88cb631f0dac2e3375e4fdd0a90027f3eef6
mannyrocky <manuel.villanueva96@hotmail.com>
2019-04-21 00:46:56 -0600

    cambios

--- login.php
+++ login.php
@@ -23,7 +23,7 @@
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header">
-				<h3>Sign In</h3>
+				<h3>Iniciar Sesion</h3>
 				<div class="d-flex justify-content-end social_icon">
 					<span><i class="fab fa-facebook-square"></i></span>
 					<span><i class="fab fa-google-plus-square"></i></span>
@@ -46,19 +46,19 @@
 						<input type="password" class="form-control" placeholder="password">
 					</div>
 					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
+						<input type="checkbox">Recordar
 					</div>
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
+							<input type="submit" value="Entrar" class="btn float-right login_btn">	
 					</div>
 				</form>
 			</div>
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="registrar.html">Sign Up</a>
+					¿No tienes una Cuenta?<a href="registrar.html">Registrate</a>
 				</div>
 				<div class="d-flex justify-content-center">
-					<a href="recuperarcontraseña.html">Forgot your password?</a>
+					<a href="recuperarcontraseña.html">¿Olvidaste tu contraseña?</a>
 				</div>
 			</div>
 		</div>
