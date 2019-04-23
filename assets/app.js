e762018086f330617e987d0e13571380fe96fd82
Fernando Inzunza <ferinzo96@gmail.com>
2019-04-23 14:56:37 -0600

    cambios

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,1 @@
 
-angular.module("rutas", ['ngRoute'])
-    .config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'template.html'
-            })
-            .when('/inicio', {
-                templateUrl: 'inicio.html'
-            })
-            .when('/usuarios', {
-                templateUrl: 'usuarios.html'
-            })
-            .when('/gestor', {
-                templateUrl: 'gestor.html'
-            })
-            .otherwise('/');
-            $locationProvider.html5Mode(true);
-    });
