921bb3dbb536fa9a3d792623dec5b1122051d403
Fernando Inzunza <ferinzo96@gmail.com>
2019-05-18 13:58:57 -0600

    cambio

--- assets/index.php
+++ assets/index.php
@@ -22,7 +22,7 @@ echo $nombre;
     <link rel="icon" type="image/png" href="assets/img/favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Proyecto</title>
-    <base href="/proyectoweb/ProgramacionWeb/assets/"/>
+    <base href="/ProgWeb/assets/"/>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
