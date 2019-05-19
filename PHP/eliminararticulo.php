ff87feac2d792790bf1868e2bee3e2110113690e
mannyrocky <manuel.villanueva96@hotmail.com>
2019-05-19 15:49:50 -0600

    eliminacion de imagen junto con el metodo de eliminar completo

--- PHP/eliminararticulo.php
+++ PHP/eliminararticulo.php
@@ -1,7 +1,22 @@
 <?php
 require_once("Clases/admin.php");
+require_once("Clases/conexion.php");
 $articulo = new Admin();
 $cod = $_POST['cod'];
+$conn = abrirBD();
+$query = "SELECT IMAGEN FROM ARTICULOS WHERE CODIGO =?";
+$sentencia_preparada1 = $conn->prepare($query);
+$sentencia_preparada1->bind_param('s',$codi);
+$codi = $cod;
+$sentencia_preparada1->execute();
+$sentencia_preparada1->bind_result($nameimagen);
+$sentencia_preparada1->fetch();
+$imageneliminar = "../assets/img/".$nameimagen;
+if(unlink($imageneliminar)){
 $articulo->EliminarArticulo($cod);
 echo "Articulo Eliminado Exitosamente";
+}else{
+    echo"no se pudo";
+}
+$conn->close();
 ?>
