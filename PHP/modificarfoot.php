4fafe41fc0ad207f494db68a32051923f420c6e1
TheIsma96 <ismael.cota.t@gmail.com>
2019-05-22 17:32:42 -0600

    cambiosismayyy

---
+++ PHP/modificarfoot.php
@@ -0,0 +1,16 @@
+<?php
+require_once('Clases/conexion.php');
+$copy = $_POST['copy'];
+$contacto = $_POST['contacto'];
+$redes = $_POST['redes'];
+$conn = abrirBD();
+$query = "UPDATE FOOTER SET COPYRIGHT = '$copy', CONTACTO = '$contacto', REDESSOCIALES = '$redes'";
+$sentencia_preparada1 = $conn->prepare($query);
+$sentencia_preparada1->bind_param('ssss',$cop,$contac,$red);
+$cop = $copy;
+$contac = $contacto;
+$red = $redes;
+$sentencia_preparada1->execute();
+echo "footer modificado Exitosamente";
+$conn->close();
+?>
