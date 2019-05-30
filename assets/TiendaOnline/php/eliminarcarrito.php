06f3032f288e35ed64b3d6dad3ad1f44be03e482
mannyrocky <manuel.villanueva96@hotmail.com>
2019-05-30 05:09:09 -0600

    carrito funcionando al 100

--- assets/TiendaOnline/php/eliminarcarrito.php
+++ assets/TiendaOnline/php/eliminarcarrito.php
@@ -4,10 +4,13 @@ session_start();
 if(isset($_SESSION['carrito'])){
     $arreglo = $_SESSION['carrito'];
     unset($arreglo[$indice]);
+    $arreglo = array_values($arreglo);
     $_SESSION['carrito'] = $arreglo;
-   
-}
-if(count($arreglo)==0){
+    
+    if(count($arreglo)==0){
         unset($_SESSION['carrito']);
     }
+   
+}
+
 ?>
