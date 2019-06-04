fb9f6534828c693403d104acd27027b1fd10f213
mannyrocky <manuel.villanueva96@hotmail.com>
2019-06-04 00:29:30 -0600

    cambios

--- assets/index.php
+++ assets/index.php
@@ -27,6 +27,10 @@ $nombre = utf8_encode($admin->Usuario);
     <base href="/ProgWeb/assets/"/>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache">
           <!-- Bootstrap core CSS     -->
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <!-- Animation library for notifications   -->
